e91af7ffc9dd296558374a81e44f201df1f2ec03
pjmantoss <joelptoss@gmail.com>
2020-03-28 17:16:14 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let canvas = document.getElementById('myCanvas');
         let ctx = canvas.getContext("2d");
-
+/*
         ctx.beginPath();
         ctx.rect(20, 40, 50, 50);
         ctx.fillStyle = "#FF0000";
@@ -18,3 +18,15 @@ let canvas = document.getElementById('myCanvas');
         ctx.strokeStyle = "rgba(0,0,255,0.5)";
         ctx.stroke();
         ctx.closePath();
+
+        */
+
+        function draw(){
+            ctx.beginPath();
+            ctx.arc(50, 50, 10, 0, Math.PI * 2);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+
+        setInterval(draw, 10);
